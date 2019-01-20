103fbd53f679f2a51d034c245df6fd5e1d9f91e9
DESKTOP-VJADCT3\Emin <emin.ahmadov99@gmail.com>
2019-01-20 13:47:58 +0100

    Impleented the requirements of the GoldenTicket class

--- GoldenTicket.java
+++ GoldenTicket.java
@@ -1,3 +1,38 @@
-public class GoldenTicket{
+import java.util.Date;
 
+public class GoldenTicket{
+	private String code;
+	private Date raffled;
+	boolean wasRaffled = false;
+	
+	//Constructors:
+	public GoldenTicket(){}
+	
+	public GoldenTicket(String code, Date raffled){
+		this.code = code;
+		this.raffled = raffled;
+	}
+
+	// Getters and Setters
+	public void setCode(String code){
+		this.code = code;
+	}
+	public String getCode(){
+		return code;
+	}
+	
+	public void setRaffled(Date raffled){
+		this.raffled = raffled;
+	}
+	public Date getRaffled(){
+		return raffled;
+	}
+	
+	public void setWasRaffled(boolean wasRaffled){
+		this.wasRaffled = wasRaffled;
+	}
+	// If the current ticket has been already raffled
+	public boolean isRaffled(){
+		return wasRaffled;
+	}
 }
