56ddf9a7037c36d1fd4bf7b765f064f6cbeeefeb
DESKTOP-VJADCT3\Emin <emin.ahmadov99@gmail.com>
2019-01-20 13:07:11 +0100

    Created OompaLoompa class

---
+++ OompaLoompa.java
@@ -0,0 +1,24 @@
+public class OompaLoompa extends Being{
+	private int height;
+	private String favoriteFood;
+	
+	public OompaLoompa(int code, String name, int height, String favoriteFood){
+		super(code, name);
+		this.height = height;
+		this.favoriteFood = favoriteFood;
+	}
+	
+	public void setHeight(int height){
+		this.height = height;
+	}
+	public int getHeight(){
+		return height;
+	}
+	public void setFavoriteFood(){
+		this.favoriteFood = favoriteFood;
+	}
+	public String getFavoriteFood(){
+		return favoriteFood;
+	}
+	
+}
