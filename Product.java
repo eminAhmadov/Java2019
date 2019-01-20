ee8a13438ddc467367b9b8342ea4b19ada45cee1
DESKTOP-VJADCT3\Emin <emin.ahmadov99@gmail.com>
2019-01-20 13:21:22 +0100

    Created Product class and fixed the GoldenTicket class

---
+++ Product.java
@@ -0,0 +1,42 @@
+public class Product{
+	String description;
+	long barcode;
+	String serialNumber;
+	GoldenTicket priceTicket;
+	
+	public Product(String description, long barcode,
+					String serialNumber, GoldenTicket priceTicket){
+		this.description = description;
+		this.barcode = barcode;
+		this.serialNumber = serialNumber;
+		this.priceTicket = priceTicket;
+	}
+	
+	public void setDescription(String description){
+		this.description = description;
+	}
+	public String getDescription(){
+		return description;
+	}
+	
+	public void setBarcode(long barcode){
+		this.barcode = barcode;
+	}
+	public long getBarcode(){
+		return barcode;
+	}
+	
+	public void setSerialNumber(String serialNumber){
+		this.serialNumber = serialNumber;
+	}
+	public String getSerialNumber(){
+		return serialNumber;
+	}
+	
+	public void setGoldenTicket(GoldenTicket priceTicket){
+		this.priceTicket = priceTicket;
+	}
+	public GoldenTicket getGoldenTicket(){
+		return priceTicket;
+	}
+}
